95bf3f07961272837922c6ff2cb6e7debe1e76a4
장혜진 <aslk3366zz@naver.com>
2021-04-10 13:54:16 +0900

    메인 화면 => 로그인 화면으로 이름 변경  - Shift +F6활용

--- app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/LoginActivity.kt
+++ app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/LoginActivity.kt
@@ -1,18 +1,16 @@
 package com.neppplus.daily10minutes_apiserverpractice_20210410
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import com.neppplus.daily10minutes_apiserverpractice_20210410.utils.ServerUtil
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_Login.*
 import org.json.JSONObject
 
-class MainActivity : BaseActivity() {
+class LoginActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_Login)
         setupEvents()
         setValues()
     }
