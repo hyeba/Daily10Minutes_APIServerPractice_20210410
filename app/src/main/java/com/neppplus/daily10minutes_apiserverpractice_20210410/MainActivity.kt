c1ef992a02f2a688827eb124dadf8c0147427ea0
장혜진 <aslk3366zz@naver.com>
2021-04-17 11:29:46 +0900

    프로젝트 목록 받아오는 기능 추가  - 메인화면에서 사용 예정

--- app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/MainActivity.kt
+++ app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/MainActivity.kt
@@ -2,8 +2,12 @@ package com.neppplus.daily10minutes_apiserverpractice_20210410
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.daily10minutes_apiserverpractice_20210410.datas.Project
+import com.neppplus.daily10minutes_apiserverpractice_20210410.utils.ServerUtil
 
 class MainActivity : BaseActivity() {
+
+    val mProjects = ArrayList<Project>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +21,17 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        서버에서 => 보여줄 프로젝트 목록이 어떤 것들이 있는지 받아서 => Project() 형태로 변환해서 => mProjects에 하나하나 추가
+
+        getProjectListFromServer()
+
+    }
+
+    fun getProjectListFromServer() {
+//        실제로 서버에서 받아오는 기능 수행
+
+        ServerUtil
 
     }
 
--- app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/utils/ServerUtil.kt
+++ app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/utils/ServerUtil.kt
@@ -1,5 +1,6 @@
 package com.neppplus.daily10minutes_apiserverpractice_20210410.utils
 
+import android.content.Context
 import android.util.Log
 import okhttp3.*
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
@@ -136,6 +137,7 @@ class ServerUtil {
 
 
         }
+//        이메일 중복체크 기능
 
         fun getRequestEmailCheck(email: String, handler: JsonResponseHandler?) {
 
@@ -158,6 +160,56 @@ class ServerUtil {
             val request = Request.Builder()
                 .url(urlString)
                 .get()
+                .build()
+
+            val client = OkHttpClient()
+
+            client.newCall(request).enqueue(object : Callback{
+                override fun onFailure(call: Call, e: IOException) {
+
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+//                response 전체 > 본문(body) 추출 (String) > JSONObject 변환 > 이 기능을 불러낸 화면에 전달
+
+                    val bodyString = response.body!!.string()
+                    val jsonObj = JSONObject(bodyString)
+                    Log.d("서버응답본문", jsonObj.toString())
+
+                    handler?.onResponse(jsonObj)
+
+                }
+
+
+
+            })
+
+        }
+
+//        프로젝트 목록 받아오기
+
+        fun getRequestProjectList(context: Context, handler: JsonResponseHandler?) {
+
+//            어디로? + 어떤 데이터? => URL을 만들 때 한꺼번에 전부 적어야 한다.
+//            주소를 적는게 복잡해짐 => 복잡한 가공을 도와주는 (OkHttp 라이브러리 제공) 클래스 활용 => URLBuilder
+
+            val urlBuilder = "${HOST_URL}/project".toHttpUrlOrNull()!!.newBuilder()
+
+//            만들어진 기초 URL에 필요한 파라미터들을 붙여주자
+//            urlBuilder.addEncodedQueryParameter("email", email)
+
+//            붙일 정보 다 붙었으면 최종 String 형태로 변환
+            val urlString = urlBuilder.build().toString()
+
+            Log.d("가공된URL", urlString)
+
+//            어디로 + 어떤 데이터? => 모두 urlString에 적혀있는 상태
+//            어떤 메쏘드? get 방식 => Request에 담아주자
+
+            val request = Request.Builder()
+                .url(urlString)
+                .get()
+                .header("X-Http-Token", ContextUtil.getLoginToken(context))
                 .build()
 
             val client = OkHttpClient()
