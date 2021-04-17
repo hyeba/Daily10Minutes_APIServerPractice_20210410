936c8b69190aee42518eeedac07b4addb507bd99
장혜진 <aslk3366zz@naver.com>
2021-04-17 15:02:40 +0900

    참여 신청

--- app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/ViewProjectDetailActivity.kt
+++ app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/ViewProjectDetailActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.neppplus.daily10minutes_apiserverpractice_20210410.datas.Project
 import com.neppplus.daily10minutes_apiserverpractice_20210410.utils.ServerUtil
@@ -31,6 +32,30 @@ class ViewProjectDetailActivity : BaseActivity() {
             ServerUtil.postRequstApplyProject(mContext, mProject.id, object : ServerUtil.JsonResponseHandler{
                 override fun onResponse(jsonObj: JSONObject) {
 
+//                    성공시 응답으로 => 새로 값이 반영된 프로젝트 JSONObject 를 다시 내려준다
+//                    새로 파싱해서 => mProject를 갱신
+
+                    val code = jsonObj.getInt("code")
+
+                    if (code == 200) {
+                        val dataObj = jsonObj.getJSONObject("data")
+                        val projectObj = dataObj.getJSONObject("project")
+
+                        mProject = Project.getProjectFromJson(projectObj)
+
+                        runOnUiThread {
+                            refreshDataToUi()
+                        }
+
+
+
+                    }
+                    else{
+                        runOnUiThread {
+                            Toast.makeText(mContext, "참여 신청에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
                 }
 
 
@@ -45,6 +70,13 @@ class ViewProjectDetailActivity : BaseActivity() {
 
         mProject = intent.getSerializableExtra("projectInfo") as Project
 
+
+
+
+
+    }
+
+    fun refreshDataToUi() {
         Glide.with(mContext).load(mProject.imageUrl).into(projectImg)
         titleTxt.text = mProject.title
         descriptionTxt.text = mProject.description
@@ -54,8 +86,9 @@ class ViewProjectDetailActivity : BaseActivity() {
         proofMethodTxt.text = mProject.proofMethod
 
 //        태그 목록은 -> 몇 개일지가 매번 다름
-//        빈 Layout을 불러서 -> 태그 갯수만큼 텍스트뷰 (코틀린에서) 추가
+//        빈 Layout을 불러서 -> 기존의 텍스트뷰 모두 삭제하고-> 태그 갯수만큼 텍스트뷰 (코틀린에서) 추가
 
+        tagListLayout.removeAllViews()
         for (tag in mProject.tags) {
 
             Log.d("프로젝트태그", tag)
@@ -70,6 +103,5 @@ class ViewProjectDetailActivity : BaseActivity() {
 
 
     }
-
 
 }
