977433ce052fbf6fb9dba2778feee0cd51b27c43
장혜진 <aslk3366zz@naver.com>
2021-04-24 14:19:52 +0900

    커스텀 액션바 반영 함수를 BaseActivity에서 만들고 메인 화면에서 적용

--- app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/BaseActivity.kt
+++ app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/BaseActivity.kt
@@ -1,6 +1,8 @@
 package com.neppplus.daily10minutes_apiserverpractice_20210410
 
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 abstract class BaseActivity : AppCompatActivity() {
 
@@ -15,6 +17,25 @@ abstract class BaseActivity : AppCompatActivity() {
 
 //        1. 액션바가 어떤 모양으로 보이게 하고싶은지?
 //         => 모양: Layout - xml 작성하자
+
+//        2. 기본 액션바 => 커스텀 액션바로 교체 작업
+
+        val defaultActionBar = supportActionBar!!
+
+//        기본액션바의 모드를 커스텀 지원모드로 변경
+        defaultActionBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+
+//        실제로 보여줄 커스텀 화면 지정(my_custom_action_bar 지정)
+
+        defaultActionBar.setCustomView(R.layout.my_custom_action_bar)
+
+//        좌우 여백이 남아서 제거해야함
+//        액션바를 잡고 있는(부모 역할) => Toolbar의 여백 제거(내부 공간을 0으로 설정)
+
+
+//        Toolbar 선택: androidx가 주는걸로 선택
+        val myToolBar = defaultActionBar.customView.parent as Toolbar
+        myToolBar.setContentInsetsAbsolute(0,0)
 
     }
 
